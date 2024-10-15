aefa8329798de301d3094d3da4816d6c18172924
whlsls3377 <whlsls3377@naver.com>
2024-10-15 17:39:06 +0900

    first commit

---
+++ src/main/java/com/sparta/springcommentcalendar/SpringCommentCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springcommentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommentCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommentCalendarApplication.class, args);
+    }
+
+}
